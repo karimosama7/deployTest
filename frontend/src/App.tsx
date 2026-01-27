1cecbc923b4ddf6f6bc53b7486c256d1a12d621b
abnaouna <no-reply@abnaouna.local>
2026-01-28 00:56:55 +0200

    Switch to HashRouter for Vercel SPA routing fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AppRoutes } from './routes/AppRoutes';
 import { ToastProvider } from './context/ToastContext';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ToastProvider>
         <AuthProvider>
           <AppRoutes />
         </AuthProvider>
       </ToastProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
